40d0f80273cee6e5986ae87b7084172d21d10f98
Hatfan Sahrul R <126065638+HansNebula@users.noreply.github.com>
2024-09-26 13:22:19 +0700

    Update ConvertibleRaceCar.cpp

--- day4/ConvertibleRaceCar.cpp
+++ day4/ConvertibleRaceCar.cpp
@@ -22,24 +22,3 @@ int main(){
     aCar.showCar();
     return 0;
 }
-
-int umur_dani = 16;
-float tinggi_dani = 163.7f
-
-int umur_danu = umur_dani + 2;
-int umur_dina = umur_danu - 4;
-
-float tinggi_rina = tinggi_dani * 0.8f;
-float tinggi_rani = tinggi_rina / 2.1f;
-
-if (nilai >= 60) {
-    std::cout << "Lulus";
-} else {
-    std::cout << "Tidak Lulus";
-}
-
-int banyak_ikan = 9;
-
-for(int i=0; i < banyak_ikan; i++){
-    std::cout << i << std::endl;
-}
